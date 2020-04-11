6a9644c8a62a7d554ab2afe46eb94f2846640052
Bijay Khatri <khatri.bijay@gmail.com>
2020-04-10 22:22:32 -0700

    add order_success page

--- index.js
+++ index.js
@@ -5,29 +5,26 @@ const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, "public")))
-  .use(bodyParser.urlencoded({
-    extended: true
-  }))
+  .use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  )
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
   .get("/about", (req, res) => res.render("pages/about"))
-  .get("/order", (req, res) => res.render("pages/order"))
+  .get("/order_success", (req, res) => res.render("pages/order_success"))
   .get("/contact", (req, res) => res.render("pages/contact"))
   .post("/submit", (req, res) => {
-    const {
-      fullName,
-      phone,
-      email,
-      items
-    } = req.body;
+    const { fullName, phone, email, items } = req.body;
     const body = req.body;
-    console.log("Going to db", {
+    console.log("Going to db...", {
       fullName,
       phone,
       email,
-      items
+      items,
     });
-    res.redirect(`/order?test=success`);
+    res.redirect(`/order_success`);
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));

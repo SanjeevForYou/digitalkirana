ac9bef5d86f90879e012307750bfd5cbfca8dedc
Bijay Khatri <khatri.bijay@gmail.com>
2020-04-11 18:29:50 -0700

    add database connection

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,10 @@
-const { Pool } = require('pg')
-const connectionString = process.env.HEROKU_POSTGRESQL_GOLD_URL,
+const { Pool } = require("pg");
+const connectionString = process.env.HEROKU_POSTGRESQL_GOLD_URL;
 const pool = new Pool({
   connectionString: connectionString,
   ssl: true,
-})
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+});
+pool.query("SELECT NOW()", (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
